4ac20df42edb6c3538d0eb4d1cc4ff9d93f824a6
José Quenede Abreu <quenede.abreu@uninorteac.com.br>
2023-07-06 15:55:52 -0500

    Rename Api

--- CompalApi/CompalApi/wsgi.py
+++ CompalApi/CompalApi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for compal project.
+WSGI config for CompalApi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CompalApi.settings')
 
 application = get_wsgi_application()
